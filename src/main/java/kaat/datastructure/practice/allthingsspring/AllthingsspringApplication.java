cc6e2951689f4ad3f0c917cd600157dd7b120e9f
kaatlev <kaatlev420@gmail.com>
2024-09-06 11:58:04 -0400

    Initial commit

---
+++ src/main/java/kaat/datastructure/practice/allthingsspring/AllthingsspringApplication.java
@@ -0,0 +1,13 @@
+package kaat.datastructure.practice.allthingsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllthingsspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllthingsspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kaat/datastructure/practice/allthingsspring/AllthingsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package kaat.datastructure.practice.allthingsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllthingsspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
